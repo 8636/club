f0d8c02348c4d7b742d85b38588c5a51405e1398
hello <dyg3966@163.com>
2019-08-22 22:23:46 +0800

    抽取github配置信息到配置文件中

--- src/main/java/cn/duan/community/controller/AuthController.java
+++ src/main/java/cn/duan/community/controller/AuthController.java
@@ -4,6 +4,7 @@ import cn.duan.community.dto.AccessTokenDTO;
 import cn.duan.community.dto.GithubUser;
 import cn.duan.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,16 +17,21 @@ public class AuthController {
 
     @Autowired
     private GithubProvider githubProvider;
-
+    @Value("${github.client.id}")
+    private String cclient_id;
+    @Value("${github.client.secret}")
+    private String client_secret;
+    @Value("${github.client.callback}")
+    private String callback;
     @GetMapping("/callback")
     public String collback(@RequestParam("code") String code,
                            @RequestParam("state") String state) throws IOException {
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
-        accessTokenDTO.setClient_id("42e591d46be31af9c854");
-        accessTokenDTO.setClient_secret("ee51a20d4185d65a1bfba0825f94daacaa9b1656");
+        accessTokenDTO.setClient_id(cclient_id);
+        accessTokenDTO.setClient_secret(client_secret);
         accessTokenDTO.setCode(code);
         accessTokenDTO.setState(state);
-        accessTokenDTO.setRedirect_uri("http://localhost:8888/callback");
+        accessTokenDTO.setRedirect_uri(callback);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser user = githubProvider.getUser(accessToken);
         System.out.println(user);
